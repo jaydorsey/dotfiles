790a34b54071576c7350d9bbf0cf68ae2d3501ea
Jay Dorsey <jaydorsey@fastmail.com>
2020-02-12 22:59:32 -0700

    Simplify iex

--- .iex.exs
+++ .iex.exs
@@ -4,32 +4,26 @@
 # IEx.configure colors: [ eval_result: [ :cyan, :bright ] ]
 Code.compiler_options(ignore_module_conflict: true)
 
-IO.puts IO.ANSI.red_background() <> IO.ANSI.white() <> " ❄❄❄ Good Luck with Elixir ❄❄❄ " <> IO.ANSI.reset
 IO.puts("<shell history is found in #{:filename.basedir(:user_cache, "erlang-history")}>")
 Application.put_env(:elixir, :ansi_enabled, true)
+
 IEx.configure(
- colors: [
-   eval_result: [:green, :bright] ,
-   eval_error: [[:red,:bright,"Bug Bug ..!!"]],
-   eval_info: [:yellow, :bright ],
- ],
- default_prompt: [
-   "\e[G",    # ANSI CHA, move cursor to column 1
-    :white,
-    "I",
-    :red,
-    "❤" ,       # plain string
-    :green,
-    "%prefix",:white,"|",
-     :blue,
-     "%counter",
-     :white,
-     "|",
-    :red,
-    "▶" ,         # plain string
-    :white,
-    "▶▶"  ,       # plain string
-      # ❤ ❤-»" ,  # plain string
-    :reset
-  ] |> IO.ANSI.format |> IO.chardata_to_string
+  colors: [
+    eval_result: [:green, :bright],
+    eval_error: [:red, :bright],
+    eval_info: [:yellow, :bright]
+  ],
+  default_prompt:
+    [
+      "\e[G",
+      :white,
+      "%prefix ",
+      :magenta,
+      # Elixir symbol from nerdfonts
+      "\ue62d",
+      " |>",
+      :reset
+    ]
+    |> IO.ANSI.format()
+    |> IO.chardata_to_string()
 )
