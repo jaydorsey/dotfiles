822e80f4cbab60fae909fbb0fb3cd8b94b891e62
Jay Dorsey <191564+jaydorsey@users.noreply.github.com>
2022-01-02 11:13:54 -0500

    Add todo-comments for neovim
    Better color highlighting of todo/note/etc.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -370,6 +370,18 @@ return require('packer').startup(function(use)
   }
 
   use {
+    'folke/todo-comments.nvim',
+    requires = 'nvim-lua/plenary.nvim',
+    config = function()
+      require('todo-comments').setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
+  use {
     'folke/which-key.nvim',
     config = function()
       require('which-key').setup {
