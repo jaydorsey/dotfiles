08805ddbe20fde6eb726ba70ccd42c1b453c5311
Jay Dorsey <191564+jaydorsey@users.noreply.github.com>
2022-02-17 17:09:51 -0500

    15 lualine

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,7 @@ require 'user.comment'
 require 'user.gitsigns'
 require 'user.nvim-tree'
 require 'user.bufferline'
+require 'user.lualine'
 
 -- This needs to load _really_ early or else it warns; just leave here for now
 vim.cmd([[
