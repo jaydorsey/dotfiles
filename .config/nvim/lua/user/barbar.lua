861c989c10158804b00ad1dfc4cba9e4d4cc8796
Jay Dorsey <191564+jaydorsey@users.noreply.github.com>
2022-06-21 21:34:29 -0400

    Fix barbar/bufferline reference
        module 'bufferline.nvim' not found

--- .config/nvim/lua/user/barbar.lua
+++ .config/nvim/lua/user/barbar.lua
@@ -1,4 +1,4 @@
-local status_ok, plugin = pcall(require, 'bufferline.nvim')
+local status_ok, plugin = pcall(require, 'bufferline')
 
 if not status_ok then
   vim.notify('Error loading user/bufferline.lua\n\n' .. plugin)
