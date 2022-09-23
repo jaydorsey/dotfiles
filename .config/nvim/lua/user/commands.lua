6b341f76b0c96e719e87fec24104fc17bae1db2c
Jay Dorsey <191564+jaydorsey@users.noreply.github.com>
2022-09-23 09:27:46 -0400

    ALEToggleFixer
    Disables the auto fixers from running since these aren't disabled when
    you disable ALE using ALEToggle (oddly)
    
    https://github.com/dense-analysis/ale/issues/1353

--- .config/nvim/lua/user/commands.lua
+++ .config/nvim/lua/user/commands.lua
@@ -6,3 +6,9 @@ vim.api.nvim_create_user_command(
 )
   -- "call fzf#vim#tags({'options': '-q '.shellescape(expand('<cword>'))})"
 -- "call fzf#vim#grep(g:rg_command .shellescape(expand('<cword>')), 1, <bang>0 ? fzf#,vim#with_preview('up:60%') : fzf#vim#with_preview('right:50%:hidden', '?'), <bang>0)",
+
+vim.api.nvim_create_user_command(
+  'ALEToggleFixer',
+  "execute \"let g:ale_fix_on_save = get(g:, 'ale_fix_on_save', 0) ? 0 : 1\"",
+  { bang = true, desc = 'Toggle ALE Fixer'}
+)
