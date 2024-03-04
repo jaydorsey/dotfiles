142934c5740c0ba00d0e54dfd0bcd03812cf738e
Jay Dorsey <191564+jaydorsey@users.noreply.github.com>
2024-03-04 08:55:22 -0500

    Ignore lock

---
+++ .config/nvim/lua/telescope.lua
@@ -0,0 +1,64 @@
+local telescope = require 'telescope'
+local fb_actions = telescope.extensions.file_browser.actions
+telescope.setup {
+  defaults = {
+    color_devicons = false,
+    layout_strategy = 'flex',
+    layout_config = { anchor = 'N' },
+    scroll_strategy = 'cycle',
+    theme = require('telescope.themes').get_dropdown { layout_config = { prompt_position = 'top' } },
+    -- ripgrep_arguments = {
+    --   'rg',
+    --   '--hidden',
+    --   '--no-heading',
+    --   '--with-filename',
+    --   '--line-number',
+    --   '--column',
+    --   '--smart-case'
+    -- }
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = 'smart_case',
+    },
+    ['ui-select'] = {
+      require('telescope.themes').get_dropdown { layout_config = { prompt_position = 'top' } },
+    },
+    heading = { treesitter = true },
+    file_browser = {
+      hijack_netwrw = true,
+      hidden = true,
+      -- mappings = {
+      --   i = {
+      --     ['<c-n>'] = fb_actions.create,
+      --     ['<c-r>'] = fb_actions.rename,
+      --     ['<c-h>'] = fb_actions.toggle_hidden,
+      --     ['<c-x>'] = fb_actions.remove,
+      --     ['<c-p>'] = fb_actions.move,
+      --     ['<c-y>'] = fb_actions.copy,
+      --     ['<c-a>'] = fb_actions.select_all,
+      --   },
+      -- },
+    },
+  },
+  pickers = {
+    buffers = {
+      ignore_current_buffer = true,
+      sort_mru = true,
+      -- sort_lastused = true,
+      previewer = false,
+    },
+  },
+}
+
+-- Extensions
+telescope.load_extension 'aerial'
+telescope.load_extension 'file_browser'
+telescope.load_extension 'fzf'
+telescope.load_extension 'heading'
+telescope.load_extension 'noice'
+telescope.load_extension 'ui-select'
+-- telescope.load_extension 'cmake_tools'
---
+++ .config/nvim/lua/treesitter.lua
@@ -0,0 +1,63 @@
+require('nvim-treesitter.configs').setup {
+  auto_install = true,
+  highlight = {
+    enable = true,
+    max_file_lines = 5000,
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+  },
+  indent = { enable = false },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<cr>',
+      node_incremental = '<tab>',
+      scope_incremental = '<cr>',
+      -- scope_decremental = '<s-cr>',
+      node_decremental = '<s-tab>',
+    },
+  },
+  refactor = {
+    smart_rename = { enable = true, keymaps = { smart_rename = 'grr' } },
+    highlight_definitions = {
+      enable = true,
+      max_file_lines = 1000,
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+    },
+  },
+  textsubjects = {
+    enable = true,
+    lookahead = true,
+    max_file_lines = 5000,
+    keymaps = {
+      ['.'] = 'textsubjects-smart',
+      [';'] = 'textsubjects-container-outer',
+      ['i;'] = 'textsubjects-container-inner',
+    },
+  },
+  endwise = { enable = true, disable = { 'noice' } },
+  matchup = {
+    enable = true,
+    include_match_words = true,
+    enable_quotes = true,
+    disable = function(lang, buf)
+      if lang == 'noice' then
+        return true
+      end
+
+      return false
+    end,
+  },
+  -- autotag = { enable = true },
+}
