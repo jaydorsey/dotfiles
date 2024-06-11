2d009e1f6e4e8a026d3fda0ee4fa186ce4794f35
Jay Dorsey <191564+jaydorsey@users.noreply.github.com>
2024-06-11 10:46:39 -0400

    Add cmp config
    100% from https://github.com/maualvm/dotfiles/blob/main/nvim/lua/plugins/cmp.lua

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -53,7 +53,112 @@ return {
       vim.cmd('colorscheme gruvbox')
     end
   },
-  { 'Exafunction/codeium.vim', lazy = false },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'saadparwaiz1/cmp_luasnip',
+      'L3MON4D3/LuaSnip',
+    },
+
+    config = function()
+      local icons = {
+        Property = " Property",
+        Variable = "α Variable",
+        Method = "󰆧 Method",
+        Function = "󰡱 Function",
+        Snippet = " Snippet",
+        Keyword = " Keyword",
+        Constant = " Constant",
+        Field = " Field",
+        File = " File",
+        Enum = " Enum",
+        EnumMember = " Enum Member",
+        Struct = " Strcut",
+        Class = " Class",
+        Interface = " Interface",
+        Module = " Module",
+        Unit = " Unit",
+        Operator = " Operator",
+        TypeParameter = " Type Parameter",
+        Reference = " Reference",
+        Folder = " Folder",
+        Color = " Color",
+        Event = " Event",
+        Value = " Value",
+        Text = " Text"
+      }
+
+      local cmp = require("cmp")
+      vim.opt.completeopt = { "menu", "menuone", "noselect" }
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<tab>"] = cmp.mapping.confirm(), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+
+        sources = cmp.config.sources(
+          {
+            { name = "codeium" },
+            { name = "nvim_lsp" },
+            { name = "nvim_lua" },
+            { name = "luasnip" }, -- For luasnip users.
+          },
+          {
+            { name = "buffer" },
+            { name = "path" },
+          }
+        ),
+        formatting = {
+          format = function(_, vim_item)
+            vim_item.kind = (icons[vim_item.kind] or "")
+            return vim_item
+          end
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end,
+
+  },
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({
+      })
+    end,
+    lazy = false
+  },
   { 'imsnif/kdl.vim' },
 
   -- { 'autozimu/LanguageClient-neovim', branch = 'next', build = 'bash install.sh' } },
@@ -229,13 +334,13 @@ return {
   { 'wincent/ferret'  }, -- Enhanced multi file search
   { 'windwp/nvim-spectre'  }, -- Regex search & replace
   { 'wsdjeg/vim-fetch' },
-  {
-    'filipdutescu/renamer.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',         -- required
-    },
-    lazy = false ,
-  },
+  -- {
+  --   'filipdutescu/renamer.nvim',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',         -- required
+  --   },
+  --   lazy = false ,
+  -- },
 
   {
     "NeogitOrg/neogit",
